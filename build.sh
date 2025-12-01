645994036ff63e5ccd95adc39f556021574af670
Omkar Kawade <kawade.omkar21@gmail.com>
2025-11-30 18:03:44 -0800

    update extractor args, bump python 3.11, update ytdlp+pkgs

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ BUILD_TAG="discord-music-bot:$GIT_SHA"
 
 echo "Building docker image"
 docker build --rm  -t "discord-music-bot:latest" -t "$BUILD_TAG" -f Dockerfile .
+
+
+echo "Running docker with build tag $BUILD_TAG"
+docker compose up
